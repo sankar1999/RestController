4c2681ee886cffe00fdea7479af312fcb1658e70
sankar1999 <sankarsathya1973@gmail.com>
2022-05-29 18:22:43 +0530

    Made some changes:

--- src/main/java/com/restcontroller/studentcontroller/studentcontroller.java
+++ src/main/java/com/restcontroller/studentcontroller/studentcontroller.java
@@ -6,8 +6,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class studentcontroller {
 
-    @GetMapping("1")
+    int intId;
+    String strName;
+    int intAge;
+    String strCountry;
+    String strReturn;
+
+    @GetMapping("studentid/1")
     public String student1() {
-        return "Student 1";
+        intId = 1;
+        strName = "Justin";
+        intAge = 28;
+        strCountry = "Canada";
+        strReturn = "Student ID: "+ intId +"\n" + "Student Name: " + strName +"\n"+ "Student Age: " + intAge + "\n"+"Student Country: " + strCountry;
+
+        return strReturn;
+    }
+
+    @GetMapping("studentid/2")
+    public String student2() {
+        intId = 2;
+        strName = "Zayn Malik";
+        intAge = 30;
+        strCountry = "UK";
+        strReturn = "Student ID: "+ intId +"\n" + "Student Name: " + strName +"\n"+ "Student Age: " + intAge + "\n"+"Student Country: " + strCountry;
+
+        return strReturn;
     }
 }
